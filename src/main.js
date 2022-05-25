496ba03382d03ec4bf3dc3f0748857e9a36ee131
root <jiuhuarfengwen@gmail.com>
2022-05-25 18:24:01 +0800

    'b'

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ Vue.config.productionTip = false
 
 var axios = require('axios')
 Vue.prototype.$axios = axios
-axios.defaults.baseURL = 'http://localhost:9797/'
+axios.defaults.baseURL = 'http://xf.express.web:9797/'
 
 /* eslint-disable no-new */
 new Vue({
